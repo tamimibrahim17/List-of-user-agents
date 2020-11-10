a86fac729f9e4036c4bbe5502e6384b51ac3b2e0
Antonio Carrasco <carrasco.acd@gmail.com>
2020-11-10 17:21:47 +0100

    Update user agents and adapt to python3

--- user-agent.py
+++ user-agent.py
@@ -5,38 +5,38 @@ import time
 url = 'http://www.useragentstring.com/pages/useragentstring.php?name=Firefox'
 
 def save(br,ua):
-	
-	file = br+'.txt'
+  
+  file = br+'.txt'
 
-	with open(file,'ab') as f:
-		f.write(ua+'\n')
+  with open(file,'a') as f:
+    f.write(ua+'\n')
 
 def getUa(br):
 
-	url = 'http://www.useragentstring.com/pages/useragentstring.php?name='+br
-	r = req.get(url)
+  url = 'http://www.useragentstring.com/pages/useragentstring.php?name='+br
+  r = req.get(url)
 
-	if r.status_code == 200:
-		soup = BeautifulSoup(r.content,'html.parser')
-	else:
-		soup = False
+  if r.status_code == 200:
+    soup = BeautifulSoup(r.content,'html.parser')
+  else:
+    soup = False
 
-	if soup:
-		div = soup.find('div',{'id':'liste'})
-		lnk = div.findAll('a')
+  if soup:
+    div = soup.find('div',{'id':'liste'})
+    lnk = div.findAll('a')
 
-		for i in lnk:
-			try:
-				save(br,i.text)
-			except:
-				print 'no ua'
-	else:
-		print 'No soup for '+br
+    for i in lnk:
+      try:
+        save(br,i.text)
+      except:
+        print('no ua')
+  else:
+    print('No soup for '+br)
 
 
 
 lst = ['Firefox','Internet+Explorer','Opera','Safari','Chrome','Edge','Android+Webkit+Browser']
 
 for i in lst:
-	getUa(i)
-	time.sleep(20)
+  getUa(i)
+  time.sleep(20)
